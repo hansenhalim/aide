9331636ff46cf18c86bfd76c1882a465810a0a6d
V Hansen Halim <fpsecond.hh@gmail.com>
2023-06-15 01:13:47 +0700

    Install headlessui and heroicons Install tailwind forms Fix dark mode Create timeline and form

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,161 @@
-<script setup></script>
-
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="bg-white overflow-hidden shadow sm:rounded-lg">
+    <div class="px-4 py-5 sm:p-6">
+      <div class="px-4 py-5 sm:p-6">
+        <h3 class="text-lg leading-6 font-medium text-gray-900">
+          Create new schedule
+        </h3>
+        <div class="mt-2 max-w-xl text-sm text-gray-500">
+          <p>Efficiently organize your time and tasks with a fresh agenda.</p>
+        </div>
+        <form class="mt-5 sm:flex sm:items-center">
+          <div class="w-full sm:max-w-xs">
+            <input
+              step="1"
+              type="datetime-local"
+              class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+            />
+          </div>
+          <div class="w-full sm:max-w-xs ml-2">
+            <input
+              type="text"
+              class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+              placeholder="Schedule #1"
+            />
+          </div>
+          <button
+            type="submit"
+            class="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+          >
+            Save
+          </button>
+        </form>
+      </div>
+      <div class="relative">
+        <div class="absolute inset-0 flex items-center" aria-hidden="true">
+          <div class="w-full border-t border-gray-300" />
+        </div>
+        <div class="relative flex justify-center">
+          <span class="bg-white px-2 text-gray-500">
+            <svg
+              class="h-5 w-5 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fill="#6B7280"
+                fill-rule="evenodd"
+                d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
+      <div class="flow-root mt-4">
+        <ul role="list" class="-mb-8">
+          <li v-for="(event, eventIdx) in timeline" :key="event.id">
+            <div class="relative pb-8">
+              <span
+                v-if="eventIdx !== timeline.length - 1"
+                class="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
+                aria-hidden="true"
+              />
+              <div class="relative flex space-x-3">
+                <div>
+                  <span
+                    :class="[
+                      event.iconBackground,
+                      'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white',
+                    ]"
+                  >
+                    <component
+                      :is="event.icon"
+                      class="h-5 w-5 text-white"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </div>
+                <div
+                  class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4"
+                >
+                  <div>
+                    <p class="text-sm text-gray-500">
+                      {{ event.content }}
+                      <a :href="event.href" class="font-medium text-gray-900">{{
+                        event.target
+                      }}</a>
+                    </p>
+                  </div>
+                  <div
+                    class="text-right text-sm whitespace-nowrap text-gray-500"
+                  >
+                    <time :datetime="event.datetime">{{ event.date }}</time>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { CheckIcon, HandThumbUpIcon, UserIcon } from "@heroicons/vue/24/solid";
+
+const timeline = [
+  {
+    id: 1,
+    content: "Applied to",
+    target: "Front End Developer",
+    href: "#",
+    date: "Sep 20",
+    datetime: "2020-09-20",
+    icon: UserIcon,
+    iconBackground: "bg-gray-400",
+  },
+  {
+    id: 2,
+    content: "Advanced to phone screening by",
+    target: "Bethany Blake",
+    href: "#",
+    date: "Sep 22",
+    datetime: "2020-09-22",
+    icon: HandThumbUpIcon,
+    iconBackground: "bg-blue-500",
+  },
+  {
+    id: 3,
+    content: "Completed phone screening with",
+    target: "Martha Gardner",
+    href: "#",
+    date: "Sep 28",
+    datetime: "2020-09-28",
+    icon: CheckIcon,
+    iconBackground: "bg-green-500",
+  },
+  {
+    id: 4,
+    content: "Advanced to interview by",
+    target: "Bethany Blake",
+    href: "#",
+    date: "Sep 30",
+    datetime: "2020-09-30",
+    icon: HandThumbUpIcon,
+    iconBackground: "bg-blue-500",
+  },
+  {
+    id: 5,
+    content: "Completed interview with",
+    target: "Katherine Snyder",
+    href: "#",
+    date: "Oct 4",
+    datetime: "2020-10-04",
+    icon: CheckIcon,
+    iconBackground: "bg-green-500",
+  },
+];
+</script>
