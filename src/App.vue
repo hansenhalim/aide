11b59fb74d237982c5121ae0df768d23ec178056
V Hansen Halim <fpsecond.hh@gmail.com>
2023-06-17 18:54:53 +0700

    wip

--- src/App.vue
+++ src/App.vue
@@ -94,14 +94,18 @@
                     <div>
                       <p class="text-sm text-gray-500">
                         {{ schedule.content }}
-                        <a href="#" class="font-medium text-gray-900">
+                        <a
+                          @click="schedule.open = true"
+                          href="#"
+                          class="font-medium text-gray-900"
+                        >
                           {{ schedule.name }}
                         </a>
                       </p>
                     </div>
                     <a
                       href="#"
-                      @click="isRelative = !isRelative"
+                      @click="toggleIsRelative()"
                       class="whitespace-nowrap text-right text-sm text-gray-500"
                     >
                       {{
@@ -115,36 +119,146 @@
                   </div>
                 </div>
               </div>
+
+              <TransitionRoot as="template" :show="schedule.open">
+                <Dialog
+                  as="div"
+                  class="fixed inset-0 z-10 overflow-y-auto"
+                  @close="schedule.open = false"
+                >
+                  <div
+                    class="flex min-h-screen items-end justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0"
+                  >
+                    <TransitionChild
+                      as="template"
+                      enter="ease-out duration-300"
+                      enter-from="opacity-0"
+                      enter-to="opacity-100"
+                      leave="ease-in duration-200"
+                      leave-from="opacity-100"
+                      leave-to="opacity-0"
+                    >
+                      <DialogOverlay
+                        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                      />
+                    </TransitionChild>
+
+                    <!-- This element is to trick the browser into centering the modal contents. -->
+                    <span
+                      class="hidden sm:inline-block sm:h-screen sm:align-middle"
+                      >&#8203;</span
+                    >
+                    <TransitionChild
+                      as="template"
+                      enter="ease-out duration-300"
+                      enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                      enter-to="opacity-100 translate-y-0 sm:scale-100"
+                      leave="ease-in duration-200"
+                      leave-from="opacity-100 translate-y-0 sm:scale-100"
+                      leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    >
+                      <div
+                        class="relative inline-block transform overflow-hidden rounded-lg bg-white text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle"
+                      >
+                        <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                          <div class="sm:flex sm:items-start">
+                            <div
+                              class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
+                            >
+                              <ExclamationTriangleIcon
+                                class="h-6 w-6 text-red-600"
+                              />
+                            </div>
+                            <div
+                              class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left"
+                            >
+                              <DialogTitle
+                                as="h3"
+                                class="text-lg font-medium leading-6 text-gray-900"
+                              >
+                                {{ schedule.content }}
+                                {{ schedule.name }}
+                              </DialogTitle>
+                              <div class="mt-2">
+                                <form
+                                  class="mt-5 sm:flex sm:items-center"
+                                  @submit.prevent="addSchedule"
+                                >
+                                  <div class="w-full sm:max-w-xs">
+                                    <input
+                                      v-model="schedule.dispatched_at"
+                                      step="1"
+                                      type="datetime-local"
+                                      class="block w-full rounded-md border-gray-300 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-sm"
+                                    />
+                                  </div>
+                                  <div
+                                    class="mt-2 w-full sm:ml-2 sm:mt-0 sm:max-w-xs"
+                                  >
+                                    <input
+                                      v-model="schedule.name"
+                                      type="text"
+                                      class="block w-full rounded-md border-gray-300 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-sm"
+                                      placeholder="Schedule #1"
+                                    />
+                                  </div>
+                                </form>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div
+                          class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"
+                        >
+                          <button
+                            type="button"
+                            class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                            @click="dispatchSchedule(schedule)"
+                          >
+                            Dispatch
+                          </button>
+                          <button
+                            type="button"
+                            class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:mt-0 sm:w-auto sm:text-sm"
+                            @click="schedule.open = false"
+                          >
+                            Cancel
+                          </button>
+                        </div>
+                      </div>
+                    </TransitionChild>
+                  </div>
+                </Dialog>
+              </TransitionRoot>
             </li>
           </ul>
         </div>
       </div>
     </div>
 
-    <div class="mt-4 overflow-hidden bg-white shadow sm:rounded-lg">
-      <div class="text-red">Clock: {{ now.format(niceFormat) }}</div>
-      <button
-        class="mt-3 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:mt-0 sm:w-auto sm:text-sm"
-        @click="syncTime"
-      >
-        syncTime
-      </button>
+    <div class="mt-4 rounded-lg bg-white p-4 shadow">
+      <span class="mt-2 font-mono text-4xl font-bold">
+        {{ now.format(niceFormat) }}
+      </span>
     </div>
   </div>
 </template>
 
 <script setup>
-import {
-  CheckIcon,
-  HandThumbUpIcon,
-  TrashIcon,
-  UserIcon,
-} from "@heroicons/vue/24/solid";
+import { CheckIcon, TrashIcon } from "@heroicons/vue/24/solid";
+import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline";
 import { ref, onMounted, onUnmounted } from "vue";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import calendar from "dayjs/plugin/calendar";
 import LocalizedFormat from "dayjs/plugin/LocalizedFormat";
+import {
+  Dialog,
+  DialogOverlay,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
 
 dayjs.extend(relativeTime);
 dayjs.extend(calendar);
@@ -152,7 +266,7 @@ dayjs.extend(LocalizedFormat);
 
 const niceFormat = "M/D/YY, h:mm:ss A";
 
-const isRelative = ref();
+const isRelative = ref(false);
 const now = ref(dayjs());
 
 const syncTime = () => {
@@ -167,29 +281,29 @@ const newSchedule = ref({
 const schedules = ref([
   {
     uuid: "51c4932d-d5dc-40a7-a517-90000c8c9bb6",
-    icon: UserIcon,
-    iconBackground: "bg-gray-400",
-    content: "Applied to",
-    name: "Build an app",
-    dispatched_at: dayjs("2023-06-18T09:00:00"),
-  },
-  {
-    uuid: "d7eebbfc-ae3d-4d34-9e71-d99307d4c62c",
-    icon: HandThumbUpIcon,
-    iconBackground: "bg-blue-500",
-    content: "Advanced to interview by",
-    name: "Go to the gym",
-    dispatched_at: dayjs("2023-06-18T09:15:00"),
-  },
-  {
-    uuid: "ce7765cd-33ec-4a62-a8be-739f1d478ec3",
     icon: CheckIcon,
     iconBackground: "bg-green-500",
-    content: "Completed interview with",
-    name: "Sleep well",
-    dispatched_at: dayjs("2023-06-18T09:30:00"),
+    content: "Dispatch",
+    name: "Schedule #1",
+    dispatched_at: dayjs("2023-06-18T09:00:00"),
+    fetch_url: "http://10.10.0.107:8088/api/?Function=Fade",
+    open: false,
+  },
+  {
+    uuid: "2207d549-a9e7-45e5-856c-bb3d729d5252",
+    icon: CheckIcon,
+    iconBackground: "bg-green-500",
+    content: "Dispatch",
+    name: "Schedule #2",
+    dispatched_at: dayjs("2023-06-18T09:00:00"),
+    fetch_url: "http://10.10.0.107:8088/api/?Function=Fade",
+    open: false,
   },
 ]);
+
+const dispatchSchedule = (schedule) => {
+  fetch(schedule.fetch_url);
+};
 
 const addSchedule = () => {
   const newScheduleObj = {
@@ -199,6 +313,8 @@ const addSchedule = () => {
     content: "Dispatch",
     name: newSchedule.value.name,
     dispatched_at: dayjs(newSchedule.value.dispatched_at),
+    fetch_url: "http://10.10.0.107:8088/api/?Function=Fade",
+    open: false,
   };
   schedules.value.push(newScheduleObj);
 };
@@ -214,10 +330,16 @@ const deleteSchedule = (schedule, index) => {
 };
 
 let syncInterval;
+let toggleTimeout;
+
+const toggleIsRelative = () => {
+  isRelative.value = !isRelative.value;
+  clearTimeout(toggleTimeout);
+};
 
 onMounted(() => {
   syncInterval = setInterval(syncTime, 500);
-  setTimeout(() => (isRelative.value = !isRelative.value), 10000);
+  toggleTimeout = setTimeout(() => (isRelative.value = true), 3000);
 });
 
 onUnmounted(() => {
